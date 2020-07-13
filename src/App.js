08c17c0850d239f7a91ecfd474c416d26531e5b7
nirajrajgor <nirajr1992@gmail.com>
2020-07-13 16:24:41 +0530

    Stored images in array for clean code. Added map to loop over them and display.

--- src/App.js
+++ src/App.js
@@ -3,17 +3,46 @@ import './App.css';
 import ImageLoad from './ImageLoad';
 import mountainRoad from "./assets/mountain-road.jpg";
 import mountainRoadSmall from "./assets/mountain-road-small.jpg";
+import forest from "./assets/forest.jpg";
+import forestSmall from "./assets/forest-small.jpg";
+import beach from "./assets/beach.jpg";
+import beachSmall from "./assets/beach-small.jpg";
+
 
 function App() {
+  const images = [
+    {
+      src: mountainRoad,
+      placeholder: mountainRoadSmall,
+      alt: "Mountain Road"
+    },
+    {
+      src: forest,
+      placeholder: forestSmall,
+      alt: "Forest"
+    },
+    {
+      src: beach,
+      placeholder: beachSmall,
+      alt: "Beach"
+    }
+  ]
   return (
     <div className="app">
       <header className="app-header">
         <h1>Progressive Load Image Using React Hooks</h1>
-        <ImageLoad
-          src={mountainRoad}
-          placeholder={mountainRoadSmall}
-          alt="Mountain Road"
-        />
+        <div className="wrapper">
+          {
+            images.map((item, i) => (
+              <ImageLoad
+                key={i}
+                src={item.src}
+                placeholder={item.placeholder}
+                alt={item.alt}
+              />
+            ))
+          }
+        </div>
       </header>
     </div>
   )
